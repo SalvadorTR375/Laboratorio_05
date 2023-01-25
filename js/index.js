48d04f26706efdcd0010e4f3b30454b6234f263b
Salvador Torres Rodriguez <a00825038@itesm.mx>
2023-01-24 22:56:57 -0600

    Los 3/3 puntos se completaron. Checar 'tachar()'

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+// Funciones implementadas en los jQuery's selectors
+
+// Punto 1)
+function AddItem(e){
+    
+    e.preventDefault()
+    let newItem = $("#newText").val()
+
+    $(".Lista").append(` <div class=recuadros> <li> ${newItem} <p></p>
+    <button class="check" type="submit">check</button> 
+    <button class="delete" type="submit">delete</button> <p></p> </li> </div> `)
+}
+
+// Punto 2)
+function tachar(){
+    let parent_li = $(this).parent()
+    parent_li.toggleClass('checked')
+}
+
+// Punto 3)
+function eliminar(){
+    let grandpa_div = $(this).parent().parent()
+    grandpa_div.remove()
+}
+
+// Definicion de jQuery Selectors
+$(".agregar").click(AddItem)
+
+$(".Lista").on("click", ".check", tachar)
+
+$(".Lista").on("click", ".delete", eliminar)
